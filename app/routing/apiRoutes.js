c02240854a114586a476c1fea8c586e1c52ba868
Eshaffer <edmond.shaffer@gmail.com>
2018-09-21 20:34:58 -0400

    api route changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,42 @@ module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friends);
     });
+    
     app.post("/api/friends", function(req, res){
+        
+        var friendInput = req.body;
+        
+        var response = friendInput.scores;
+
+        var friendName = "";
+        
+        var friendImage = "";
+        
+        var difference = 150
+
+        for (var i = 0; i < friends.length; i++) {
+            
+            var different = 0;
+            
+            for(var j = 0; j < response.length; j++){
+                
+                different += Math.abs(friends[i].scores[j] - responses[j]);
+            } 
+            if (different < difference) {
+                
+                difference = different;
+                
+                friendName = friends[i].name;
+                
+                friendImage = friends[i].image;
+			}
+        }
+        res.json({
+            name: friendName,
+            image: friendImage
+        });
+
+        friends.push(friendInput);
 
     })
 
