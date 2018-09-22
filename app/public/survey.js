aae2892123eb3dc640de4c90ab2e28e4814cd9ce
Eshaffer <edmond.shaffer@gmail.com>
2018-09-21 20:36:09 -0400

    html changes

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,14 +1,13 @@
-M.AutoInit();
-
 $(".submitBtn").on("click", function (event) {
     event.preventDefault();
 
     var validate = true;
+    
     if ($("#name").val() === "" || $("image").val() === "") {
         validate = false;
 
     }if ($(".q1").val() === "empty" || $(".q2").val() === "empty" || $(".q3").val() === "empty" || $(".q4").val() === "empty" || $(".q5").val() === "empty" || $(".q6").val() === "empty" || $(".q7").val() === "empty" || $(".q8").val() === "empty" || $(".q9").val() === "empty" || $(".q10").val() === "empty") {
-        valid = false;
+        validate = false;
         alert("Please answer all of the questions to find the best match");
    
     } else {
@@ -41,3 +40,5 @@ $(".submitBtn").on("click", function (event) {
     };
  
 });
+
+M.AutoInit();
