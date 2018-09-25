fef86ce1156d7b3643b948d597a375fd3b55980c
Eshaffer <edmond.shaffer@gmail.com>
2018-09-25 18:58:26 -0400

    validate works

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,23 +2,23 @@ var friends = [{
     name:"Miley Cirus",
     photo:"http://cdn.akc.org/content/article-body-image/funny-basset_hound_yawning.jpg",
     scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        '5',
+        '1',
+        '4',
+        '4',
+        '5',
+        '1',
+        '2',
+        '5',
+        '4',
+        '1'
       ]
   },{
     name: 'Brad Pitt ',
 		photo: 'http://brewminer.com/photos/1441184974_91cfb179ce_m.png',
 		scores: [
-			2,
-			4,
+			'2',
+			'4',
 			'1',
 			'1',
 			'1',
@@ -49,31 +49,31 @@ var friends = [{
     name:"Donald",
     photo:"https://i.pinimg.com/originals/d4/c2/00/d4c200c9ed401c0fb445b0f56d2b09a7.jpg",
     scores:[
-        3,
-        1,
-        2,
-        4,
-        1,
-        1,
-        2,
-        3,
-        4,
-        5
+        '3',
+        '1',
+        '2',
+        '4',
+        '1',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5'
       ]
   },{
     name:"Tom Cruise",
     photo:"https://i.pinimg.com/originals/d4/c2/00/d4c200c9ed401c0fb445b0f56d2b09a7.jpg",
     scores:[
-        3,
-        1,
-        2,
-        4,
-        1,
-        1,
-        2,
-        3,
-        4,
-        5
+        '3',
+        '1',
+        '2',
+        '4',
+        '1',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5'
       ]
   }]
 // Note how we export the array. This makes it accessible to other files using require.
